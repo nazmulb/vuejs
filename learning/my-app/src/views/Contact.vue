c8391999f227bc3204e4e8bd110c8e63d6f3a559
nazmulbasher <nazmul.basher@fieldnation.com>
2020-04-02 14:28:08 +0300

    added codes

---
+++ learning/my-app/src/views/Contact.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>{{ h1Text }}</h1>
+        <input v-model="h1Text">
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            h1Text: "This is an contact page"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
