8021a5c13785cfe10ac725f02b78e19ab62e3b9f
nazmulbasher <nazmul.basher@fieldnation.com>
2020-04-03 18:11:12 +0300

    added codes

--- learning/my-app/src/views/About.vue
+++ learning/my-app/src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Kilometers:
+      <input type="text" v-model="kilometers" />
+      Meters:
+      <input type="text" v-model="meters" />
+    </p>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      kilometers: 0,
+      meters: 0
+    };
+  },
+  watch: {
+    kilometers: function(val) {
+      this.kilometers = val;
+      this.meters = val * 1000;
+    },
+    meters: function(val) {
+      this.kilometers = val / 1000;
+      this.meters = val;
+    }
+  }
+};
+</script>
