2545bc42cda75073e0fd6f1a133ea6dfe2f48d5f
nazmulbasher <nazmul.basher@fieldnation.com>
2020-04-30 20:20:55 +0300

    added codes

--- learning/my-app-ts/src/components/HelloWorld.vue
+++ learning/my-app-ts/src/components/HelloWorld.vue
@@ -1,129 +1,79 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    {{ firstName }} {{ lastName }}
+    <input v-model.trim="name" />
     <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
+      <button @click.prevent="decrement">-</button>
+      {{ count }}
+      <button @click.prevent="increase">+</button>
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
-          target="_blank"
-          rel="noopener"
-          >typescript</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-          >vuex</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest"
-          target="_blank"
-          rel="noopener"
-          >unit-jest</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+    <p>
+      Kilometers:
+      <input type="text" v-model="kilometers" />
+      Meters:
+      <input type="text" v-model="meters" />
+    </p>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
+import { Component, Prop, Watch, Vue } from "vue-property-decorator";
 
 @Component
 export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
+  @Prop({ required: false, type: String, default: "Welcome" })
+  private readonly msg!: string;
+
+  // Class properties will be component data
+  private count: number = 0;
+  private firstName: string = "Nazmul";
+  private lastName: string = "Basher";
+  private kilometers: number = 0;
+  private meters: number = 0;
+
+  // Methods will be component methods
+  public decrement() {
+    this.count--;
+  }
+
+  public increase() {
+    this.count++;
+  }
+
+  // Declared as computed property getter
+  get name() {
+    return this.firstName + " " + this.lastName;
+  }
+
+  // Declared as computed property setter
+  set name(value: string) {
+    const splitted = value.split(" ");
+    console.log(splitted);
+    this.firstName = splitted[0];
+    this.lastName = splitted[1] || "";
+  }
+
+  @Watch("kilometers")
+  kilometersChange(value: number) {
+    this.kilometers = value;
+    this.meters = value * 100;
+  }
+
+  @Watch("meters")
+  metersChange(value: number) {
+    this.kilometers = value / 100;
+    this.meters = value;
+  }
+
+  // Declare mounted lifecycle hook
+  created() {
+    console.log("created!");
+  }
+
+  mounted() {
+    console.log("mounted!");
+  }
 }
 </script>
 
